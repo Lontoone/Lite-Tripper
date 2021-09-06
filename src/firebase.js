99cc995a718f5b5f870ab6bca33f9ba572f41125
cingjia <news2000tw@gmail.com>
2021-09-07 04:26:18 +0900

    add firebase build

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCdWFxIUbjchiqeNO3L8LtNp8JyMfCatKI",
+    authDomain: "lite-tripper.firebaseapp.com",
+    projectId: "lite-tripper",
+    storageBucket: "lite-tripper.appspot.com",
+    messagingSenderId: "619686803558",
+    appId: "1:619686803558:web:b19dcc30395aad4ccdd5a7",
+    measurementId: "G-SYCHJT3HW1"
+  };
